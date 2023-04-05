e410e96e21ae3d4d5d0cf61688e17df6b2e762d1
SalJelena <salopekjelena24@gmail.com>
2023-04-05 12:11:13 +0200

    edit API

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -12,7 +12,7 @@ const AroundYou = () => {
 
     useEffect(() => {
 
-        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_FE0LBCZJpiy120qZfw0Ss4QzTgrZx`)
+        axios.get(`GEO IPIFY`)
         .then((res) => setCountry(res?.data?.location?.country))
         .catch((err) => console.log(err))
         .finally(() => setLoading(false))
